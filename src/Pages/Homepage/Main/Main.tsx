2ae3213efc9426dcb1b04f9c30d42dd0c6f14f4d
MBerry97 <imatty2011@hotmail.co.uk>
2021-11-08 17:59:12 +0000

    implemented project layout

---
+++ src/Pages/Homepage/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductImages from './ProductImages/ProductImages';
+import ProductInfo from './ProductInfo/ProductInfo';
+
+const Main = () => {
+  return (
+    <main>
+      <ProductImages />
+      <ProductInfo />
+    </main>
+  );
+};
+
+export default Main;
