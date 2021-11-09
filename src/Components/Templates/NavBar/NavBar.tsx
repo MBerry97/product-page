d7889364d0e3d44d77f9cc6774c1b793ff1d9a3b
MBerry97 <imatty2011@hotmail.co.uk>
2021-11-09 12:15:09 +0000

    added html and css positioning for nav layout

--- src/Components/Templates/NavBar/NavBar.tsx
+++ src/Components/Templates/NavBar/NavBar.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import logo from '../../../Assets/images/logo.svg';
+import burgerMenu from '../../../Assets/images/icon-menu.svg';
+import cart from '../../../Assets/images/icon-cart.svg';
+import avatar from '../../../Assets/images/image-avatar.png';
+import './NavBar.css';
 
 const NavBar: React.FC = (): JSX.Element => {
-  return <div className="navBar">navbar</div>;
+  return (
+    <div className="navBar">
+      <div>
+        <img src={burgerMenu} alt="menu" />
+        <img src={logo} alt="logo" />
+      </div>
+      <div>
+        <img src={cart} alt="cart" />
+        <img src={avatar} alt="avatar" />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
