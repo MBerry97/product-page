701ff6d18a0b76fdf834fb451c7a2c871592781f
MBerry97 <imatty2011@hotmail.co.uk>
2021-11-09 11:29:43 +0000

    created wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
+import Wrapper from './Components/Containers/Wrapper/Wrapper';
 import HomePage from './Pages/Homepage/HomePage';
 
 const App: React.FC = (): JSX.Element => {
   return (
-    <div className='App'>
-      <HomePage />
+    <div className="App">
+      <Wrapper additionalClass="page-wrapper">
+        <HomePage />
+      </Wrapper>
     </div>
   );
 };
---
+++ src/Components/Containers/Wrapper/Wrapper.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Wrapper.css';
+
+type IProps = {
+  children: JSX.Element;
+  additionalClass: string;
+};
+
+const Wrapper: React.FC<IProps> = ({
+  children,
+  additionalClass,
+}): JSX.Element => {
+  return <div className={`wrapper ${additionalClass}`}>{children}</div>;
+};
+
+export default Wrapper;
