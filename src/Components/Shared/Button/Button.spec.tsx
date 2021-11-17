218da01ce28ac273c7c3e68ba7a959394d900bb6
MBerry97 <imatty2011@hotmail.co.uk>
2021-11-17 16:54:10 +0000

    added testing for button component

--- src/Components/Shared/Button/Button.spec.tsx
+++ src/Components/Shared/Button/Button.spec.tsx
@@ -1,12 +1,34 @@
+/* eslint-disable testing-library/no-render-in-setup */
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Button from './Button';
 
-describe('<Button />', () => {
-  it('Renders the button component', () => {
-    render(<Button text="test" type="primary" />);
+const onClickHandler = jest.fn();
 
+describe('<Button />', () => {
+  beforeEach(() => {
+    render(
+      <Button
+        text="test"
+        type="primary"
+        img={<img src="test" alt="test" />}
+        onClickFn={onClickHandler}
+      />
+    );
+  });
+  it('Renders the button component', () => {
     const button = screen.getByRole('button');
 
     expect(button).toBeInTheDocument();
+  });
+  it('Buttons inner content contains image element and text  passed down as props', () => {
+    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getByText('test')).toBeInTheDocument();
+  });
+  it('Buttons onClick event handle is trigger on click', () => {
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    expect(onClickHandler).toHaveBeenCalledTimes(1);
   });
 });
