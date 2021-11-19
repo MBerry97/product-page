4a463a4a87b4f04e06a0b723a99859d9406abf38
MBerry97 <imatty2011@hotmail.co.uk>
2021-11-19 11:46:20 +0000

    added testing for lightbox

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-
   moduleNameMapper: {
-    '\\.css$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/fileMock.js',
+    '\\.(css|scss)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['./src/jest.setup.ts'],
 };
---
+++ src/Contexts/appContext.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import React from 'react';
+
+// @ts-ignore:
+export const appContext = React.createContext<any>({} as any);
+
+export const { Provider } = appContext;
